6c4894a547906ffd1121486e301775221bf1e802
Hongjun Lee <zunza96@gmail.com>
2024-04-12 16:50:13 +0900

    [INIT]: first commit

---
+++ src/main/java/com/goldenplanet/licenseKeyAuth/LicenseKeyAuthApplication.java
@@ -0,0 +1,13 @@
+package com.goldenplanet.licenseKeyAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseKeyAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseKeyAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goldenplanet/licenseKeyAuth/LicenseKeyAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldenplanet.licenseKeyAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseKeyAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
